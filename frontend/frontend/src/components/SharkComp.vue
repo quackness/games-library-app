14d4b7037b3d5dab2a186f7f4432e239aee3d566
Karolina Redden <dubajkaro@gmail.com>
2023-04-06 20:05:48 -0300

    add axios and crreated() lifecycle

--- frontend/frontend/src/components/SharkComp.vue
+++ frontend/frontend/src/components/SharkComp.vue
@@ -5,13 +5,30 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
   name: "SharkComp",
   data() {
     return {
       msg: "",
     };
+  },
+  methods: {
+    getResponse() {
+      const path = "http://localhost:5000/shark";
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.msg = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getResponse();
   },
 };
 </script>
