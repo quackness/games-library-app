b3060cb1e11c8f85ae448988f70478364210c0aa
Karolina Redden <dubajkaro@gmail.com>
2023-04-06 12:40:37 -0300

    change component name to use multi-word convention

---
+++ frontend/frontend/src/components/SharkComp.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SharkComp",
+  data() {
+    return {
+      msg: "Hello, this is Shark",
+    };
+  },
+};
+</script>
