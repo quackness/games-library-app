843436a02e79141d5e5ac6ec389d71114e2976c3
Karolina Redden <dubajkaro@gmail.com>
2023-04-10 12:35:01 -0300

    add cdn for bootswatch

--- frontend/frontend/src/components/GamesList.vue
+++ frontend/frontend/src/components/GamesList.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="jumbotron vertical-center">
     <div class="container">
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css"
+        integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R"
+        crossorigin="anonymous"
+      />
       <p>Games Library 🕹️</p>
     </div>
   </div>
