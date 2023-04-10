cc4d082b265f4adc5e25718918192a977e4f3e9b
Karolina Redden <dubajkaro@gmail.com>
2023-04-10 13:02:43 -0300

    add table for games

--- frontend/frontend/src/components/GamesList.vue
+++ frontend/frontend/src/components/GamesList.vue
@@ -7,7 +7,37 @@
         integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R"
         crossorigin="anonymous"
       />
-      <p>Games Library 🕹️</p>
+      <div class="row">
+        <div class="col-sm-12">
+          <p>Games Library 🕹️</p>
+          <hr />
+          <br />
+          <!-- alert message -->
+          <button type="button" class="btn btn-success btn-sm">Add game</button>
+          <br />
+          <br />
+          <table class="table table-hover">
+            <!-- table head -->
+            <thead>
+              <tr>
+                <!-- table header cells  -->
+                <th scope="col">Title</th>
+                <th scope="col">Genre</th>
+                <th scope="col">Played?</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>DO</td>
+                <td>RE</td>
+                <td>MI</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
 </template>
